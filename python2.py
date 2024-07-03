15e07dcd4cb9d5518300376be707650ee5ba6e0f
Pierre de Thoisy <113915383+pierredethoisy@users.noreply.github.com>
2024-07-03 19:53:49 +0200

    Create python2.py

---
+++ python2.py
@@ -0,0 +1,13 @@
+import MySQLdb, pprint
+
+uneConnexionBDD = MySQLdb.connect
+(host   ='192.32.12.10',
+user   ='admin',
+password = "cZIxOB71IFGoRLDSFG11222333444135676SFS2343546123124134123124",
+db     ='uneBase')
+leCurseur       = uneConnexionBDD.cursor()
+unAuteur        = "'Zola'"
+pprint.pprint(leCurseur.fetchall())
+leCurseur.query("update books set title='assommoir' where author='Zola'")
+leCurseur.execute(""" SELECT title, description FROM books WHERE author = %s """ % (unAuteur,))
+uneConnexionBDD.commit()
