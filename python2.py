a6ae27a5722719480d6da94dbe4c7745a9f7d1c2
Pierre de Thoisy <pierre.dethoisy@gitguardian.com>
2024-09-25 22:26:31 +0200

    Update python2.py

--- python2.py
+++ python2.py
@@ -11,3 +11,10 @@ pprint.pprint(leCurseur.fetchall())
 leCurseur.query("update books set title='assommoir' where author='Zola'")
 leCurseur.execute(""" SELECT title, description FROM books WHERE author = %s """ % (unAuteur,))
 uneConnexionBDD.commit()
+
+
+(host   ='192.32.12.10',
+user   ='admin',
+password = "cZIxOB71IFGoRLDSFG11222333444135676SFS234354612312413412QSDQSFQSF",
+db     ='uneBase')
+leCurseur       = uneConnexionBDD.cursor()
